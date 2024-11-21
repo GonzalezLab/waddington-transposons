07cc212ae6214d9fdace7e0f59d8a0fce031f57a
Marta <marta.coronado@uab.cat>
2024-11-21 09:16:33 +0100

    Add files via upload

---
+++ 5. Post-analysis/analysis_segregating.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+module load cesga/system miniconda3/22.11.1-1
+conda activate waddington
+
+# Run
+
+DIR="/home/csic/gcy/mcz/scratch/Waddington/FIX"
+
+cd $DIR
+
+types="D907_A D907_N Mix_A Mix_N Mix2_A Mix2_N"
+
+awk '$3 == "gene" '  ${DIR}/referenceGenome/dmel-all-r6.31.gtf | grep -E "^2L|^2R|^3L|^3R|^X" | sort -k1,1 -k4,4n -k5,5n > ${DIR}/referenceGenome/dmel-all-r6.31.gene.gtf
+
+mkdir -p ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/
+mkdir ${DIR}/ANALYSES/post_analysis_R01/segregatingTEresults/
+
+for type in $types; do
+
+> ${DIR}/ANALYSES/post_analysis_R01/segregatingTEresults/segregatingTE.infoIntersect.allGenes.$type.tab
+
+if [[ "$type" == "D907_A" ]]
+then
+awk '$12 < 0.01' ${DIR}/ANALYSES/post_analysis_R01/result_FC_fisher_D907_A.tab  | grep -E "A-|-A" | grep -E "P0-|C-" | awk -F'\t' '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $1 "\t" "TEMP2" "\t" $8 }' > ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/$type.segregatingTE.bed
+elif [[ "$type" == "D907_N" ]]
+then
+awk '$12 < 0.01' ${DIR}/ANALYSES/post_analysis_R01/result_FC_fisher_D907_N.tab  | grep -E "N-|-N" | grep -E "P0-|C-" | awk -F'\t' '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $1 "\t" "TEMP2" "\t" $9}'> ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/$type.segregatingTE.bed
+elif [[ "$type" == "Mix_A" ]]
+then
+awk '$12 < 0.01' ${DIR}/ANALYSES/post_analysis_R01/result_FC_fisher_Mix_A.tab  | grep -E "A-|-A" | grep -E "P0-|C-" | awk -F'\t' '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $1 "\t" "TEMP2" "\t" $8}'> ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/$type.segregatingTE.bed
+elif [[ "$type" == "Mix_N" ]]
+then
+awk '$12 < 0.01' ${DIR}/ANALYSES/post_analysis_R01/result_FC_fisher_Mix_N.tab  | grep -E "N-|-N" | grep -E "P0-|C-" | awk -F'\t'  '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $1 "\t" "TEMP2" "\t" $9}'> ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/$type.segregatingTE.bed
+elif [[ "$type" == "Mix2_A" ]]
+then
+awk '$12 < 0.01' ${DIR}/ANALYSES/post_analysis_R01/result_FC_fisher_Mix2_A.tab  | grep -E "A-|-A" | grep -E "P0-|C-" | awk -F'\t'  '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $1 "\t" "TEMP2" "\t" $8}'> ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/$type.segregatingTE.bed
+elif [[ "$type" == "Mix2_N" ]]
+then
+awk '$12 < 0.01' ${DIR}/ANALYSES/post_analysis_R01/result_FC_fisher_Mix2_N.tab  | grep -E "N-|-N" | grep -E "P0-|C-" | awk -F'\t' '{print $3 "\t" $4 "\t" $5 "\t" $2 "\t" $1 "\t" "TEMP2" "\t" $9}'> ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/$type.segregatingTE.bed
+fi
+
+type2="$type"
+if [[ "$type" == "Mix2_A" ]]; then
+type2="Mix_A"
+elif [[ "$type" == "Mix2_N" ]]; then
+type2="Mix_N"
+fi
+
+i=1
+
+# Read each line in the input file
+while read insertion; do
+
+# Save the current insertion to a temporary BED file
+echo "$insertion" > ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.bed 
+
+# Run bedtools window with a 1kb window
+bedtools window -a ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.bed \
+-b ${DIR}/referenceGenome/dmel-all-r6.31.gene.gtf \
+-w 1000 > ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.intersect 
+#cat ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.intersect 
+
+# Check if the bedtools window output is empty
+if [ ! -s ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.intersect  ]; then
+# If empty, run bedtools closest instead
+bedtools closest -a ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.bed \
+-b ${DIR}/referenceGenome/dmel-all-r6.31.gene.gtf -d > ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.window 
+# gene=$(cat ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.window | cut -f 16 | cut -f 2 -d' ' | tr -d '"' | tr -d ';')
+# distance=$(cat ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.window | cut -f 17 ) 
+
+# if grep -q "$gene" $DIR/list_genes_DE/genes_*_${type2}.lst; then
+# DEG=1
+# else
+# DEG=0
+# fi
+#result=$(echo "${gene}:${distance}bp ($DEG)")
+genes=$(cat ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.window   | cut -f 16 | cut -f 2 -d' ' | tr -d '"' | tr -d ';')
+result=""
+
+while read gene
+do
+grep -w $gene ${DIR}/referenceGenome/dmel-all-r6.31.gene.gtf > ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.gene.$gene.gff
+distance=$(bedtools closest -a ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.bed -b ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.gene.$gene.gff -d | cut -f 17 )
+
+DEG=""
+
+# Loop through all matching files
+for file in $DIR/list_genes_DE/genes_*_${type2}.lst; do
+    if grep -q "$gene" "$file"; then
+        # Extract the condition from the filename and append to DEG
+        case=$(basename "$file" | sed -E "s/genes_([A-Za-z]+_[A-Za-z]+)_${type2}\.lst/\1/")
+        if [[ -n "$DEG" ]]; then
+            DEG="$DEG, $case"
+        else
+            DEG="$case"
+        fi
+    fi
+done
+
+# If DEG is still empty, set it to "0"
+if [[ -z "$DEG" ]]; then
+    DEG=0
+fi
+
+result=$(echo "${gene}:${distance}bp ($DEG);$result")
+done <<< "$genes"
+
+else
+genes=$(cat ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.intersect  | cut -f 16 | cut -f 2 -d' ' | tr -d '"' | tr -d ';')
+result=""
+
+while read gene
+do
+grep -w $gene ${DIR}/referenceGenome/dmel-all-r6.31.gene.gtf > ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.gene.$gene.gff
+distance=$(bedtools closest -a ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.bed -b ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.gene.$gene.gff -d | cut -f 17 )
+
+DEG=""
+
+# Loop through all matching files
+for file in $DIR/list_genes_DE/genes_*_${type2}.lst; do
+    if grep -q "$gene" "$file"; then
+        # Extract the condition from the filename and append to DEG
+        case=$(basename "$file" | sed -E "s/genes_([A-Za-z]+_[A-Za-z]+)_${type2}\.lst/\1/")
+        if [[ -n "$DEG" ]]; then
+            DEG="$DEG, $case"
+        else
+            DEG="$case"
+        fi
+    fi
+done
+
+# If DEG is still empty, set it to "0"
+if [[ -z "$DEG" ]]; then
+    DEG=0
+fi
+
+result=$(echo "${gene}:${distance}bp ($DEG);$result")
+done <<< "$genes"
+fi
+
+H3K27ac=""
+bedtools closest \
+-a ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp/$type.segregatingTE.$i.bed \
+-b ${DIR}/ANALYSES/post_analysis_R01/H3K27ac_allMixLinesMerged.fix.bed -d \
+> ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp//$type.segregatingTE.$i.closest_H3K27ac.bed
+H3K27ac=$(cat ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/tmp//$type.segregatingTE.$i.closest_H3K27ac.bed | cut -f8-11  | awk -F'\t' '{print $1 ":" $2 "-" $3 "=" $4}')
+
+
+
+# Chr Start End TEName PopName Freq Gene Distance
+infoIns=$(echo "$insertion" | cut -f 1-5,7)
+echo -e "$infoIns\t$result\t$H3K27ac" >> ${DIR}/ANALYSES/post_analysis_R01/segregatingTEresults/segregatingTE.infoIntersect.allGenes.$type.tab
+
+# Increment the counter
+i=$(( i + 1 ))
+
+done < ${DIR}/ANALYSES/post_analysis_R01/segregatingTE/$type.segregatingTE.bed
+
+done 
+
+
+
+
+
+
+# cruzar con genes 1kb
+# mas lejos si no hay (closest)
+# cruzar DEG
+# cruzar TE con enhancer
+# mirar que gen es
+
